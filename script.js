25504a5b8902611e06e4d7360be8059aeb64e377
Matrayu <northridge614@gmail.com>
2019-01-19 09:38:13 -0800

    added basic requirements including stretch goal of address to JS

--- script.js
+++ script.js
@@ -1,43 +1,89 @@
 'use strict';
 
-const url = 'https://developer.nps.gov/api/v1/parks?';
-const api = 'api_key=gDz5lLLWu7hY98JWSKkGvavTYAjeRj0SLI8uX7JL'
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
+const parksApi = 'gDz5lLLWu7hY98JWSKkGvavTYAjeRj0SLI8uX7JL'
+const googleGeoApi = 'AIzaSyCQIlfCNs73wljmt2yBoHkM_YuMZOXA34g'
 
 function watchForm() {
     console.log('watchForm ran');
     $('form').submit(event => {
         event.preventDefault();
-        let state = getState();
-        let numResults = getMaxNumber();
-        getParks(url, api, state, numResults)
+        let state = $("#js-state").val();
+        let numResults = $("#js-maxResults").val();
+        getParks(state, numResults)
     })
 }
 
-function getState() {
-    console.log('getState has run');
-    return $("#js-state").val();
-}
-
-function getMaxNumber() {
-    console.log('getMaxNumber has run');
-    return $("#js-maxResults").val();
-}
-
-function getParks(url, api, state, limit) {
+function getParks(state, limit=10) {
     console.log('getParks ran');
-    let queryString = `${url}statecode=${state}&limit=${limit}&${api}`
-    fetch(queryString)
+    const params = {
+        api_key: parksApi,
+        limit,
+        fields: "addresses",
+        statecode: state,
+    };
+
+    const newQueryString = formartQueryParams(params)
+    const url = `${searchURL}?${newQueryString}`
+
+    console.log(url)
+
+    fetch(url)
         .then(response => {
             if (response.ok) {
                 return response.json();
             }
             throw new Error(response.statusText);
         })
-        .then(responseJson => console.log(responseJson))
+        .then(responseJson => getResultsArray(responseJson))
         .catch(err => {
-            $('.#js-error-message').text(`Something went wrong: ${err.message}`);
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
-};
+}
+
+function formartQueryParams(obj) {
+    console.log('formartQueryParams ran')
+    const queryParms = Object.keys(obj).map(key => 
+        `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])
+        }`);
+    return queryParms.join('&');
+}
+
+function getResultsArray(arr) {
+    console.log('getResultsArray ran');
+    $(".parks").empty();
+    let parks = '';
+    for(let i = 0; i < arr.data.length - 1; i++) {
+        $('.parks').append(`
+            <div class="park-results">
+                <h2>${arr.data[i].fullName}</h2>
+                <p>${arr.data[i].description}</p>
+                <p>${getAddress(arr.data[i].addresses)}</p>
+                <a href="${arr.data[i].url}" target="_blank">Link to ${arr.data[i].name} Park website</a> 
+            </div>`)
+    };
+    $('.results').removeClass('hidden')
+}
+
+function getAddress(arr) {
+    console.log('getAddress ran')
+    console.log(arr)
+    let address = ''
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i].type = "Physical") {
+            address = address + `${arr[i].line1}<br>`
+            if (arr[i].line2 != "") {
+                console.log('blank line two');
+                address = address + `${arr[i].line2}<br>${arr[i].city} ${arr[i].stateCode}<br>${arr[i].postalCode}`
+            }
+            else {
+                console.log('not blank');
+                address = address + `${arr[i].city} ${arr[i].stateCode}<br>${arr[i].postalCode}`
+            };
+        };
+    };
+    return(address);
+}
 
 
 $(watchForm);
